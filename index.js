bfa4ff6a1b3047f54e015a9598e118c7a69fc3af
Brody Dingel <brodydingel795@gmail.com>
2020-12-30 21:18:04 -0600

    Add timestamps to logs

--- index.js
+++ index.js
@@ -9,20 +9,21 @@ const handler = async (req, h) => {
 
     try {
         if (!project || !env || !password) {
-            console.log(`Failed to pass in necessary payload. Payload received: ${JSON.stringify(req.payload)}`);
+            console.log(`${new Date()} Failed to pass in necessary payload. Payload received: ${JSON.stringify(req.payload)}`);
             return h.response('Failed to pass in necessary payload').code(400);
         }
 
         await exec(`bash ${scriptPath}`);
         
+        console.log(`${new Date()} Successfully ran deployment script at ${scriptPath}`);
         return h.response(`Successfully ran deployment script at ${scriptPath}`).code(200);
     } catch (error) {
         if (error.message.includes('No such file or directory')) {
-            console.log(`Deployment script not found at path ${scriptPath}`);
+            console.log(`${new Date()} Deployment script not found at path ${scriptPath}`);
             return h.response('Deployment script not found at specified path.').code(400);
         }
 
-        console.log(`An unhandled error occurred. ${error}`);
+        console.log(`${new Date()} An unhandled error occurred. ${error}`);
         return h.response('An unhandled error occurred').code(400);
     }
 }
