dad322dd5e48c2f427d4687f69a0240f1702bac9
Brody Dingel <brodydingel795@gmail.com>
2020-12-30 21:14:43 -0600

    Add better logging and responses

--- index.js
+++ index.js
@@ -3,24 +3,27 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 require('dotenv').config();
 
-const handler = async (request) => {
-    const {project, env, password} = request.payload;
+const handler = async (req, h) => {
+    const {project, env, password} = req.payload;
     const scriptPath = `${process.env.COMMON_PATH}/${project}/${env}/${project}/${process.env.SCRIPT_NAME}`;
 
     try {
         if (!project || !env || !password) {
-            return `Failed to pass in necessary payload. Payload received: ${JSON.stringify(request.payload)}`;
+            console.log(`Failed to pass in necessary payload. Payload received: ${JSON.stringify(req.payload)}`);
+            return h.response('Failed to pass in necessary payload').code(400);
         }
 
         await exec(`bash ${scriptPath}`);
         
-        return `Successfully ran deployment script at ${scriptPath}`;
+        return h.response(`Successfully ran deployment script at ${scriptPath}`).code(200);
     } catch (error) {
         if (error.message.includes('No such file or directory')) {
-            return `File not found at path ${scriptPath}`;
+            console.log(`Deployment script not found at path ${scriptPath}`);
+            return h.response('Deployment script not found at specified path.').code(400);
         }
 
-        return `An unhandled error occurred. ${error}`
+        console.log(`An unhandled error occurred. ${error}`);
+        return h.response('An unhandled error occurred').code(400);
     }
 }
 
